ff5bff3d8546a730fc62f50e2fac43cf9516cbdb
Apagyi Dávid <apagyi.david@gmail.com>
2018-02-17 14:49:09 +0100

    var

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ app.post('/upload', function(req, res) {
   if (!req.files)
   return res.status(400).send('No files were uploaded.');
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+  var sampleFile = req.files.sampleFile;
   // Fájl áthelyezése
   sampleFile.mv(dir+sampleFile.name, function(err) {
     if (err)
