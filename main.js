5f9d837f41acc2c45dfd79a648fcca758f3d0a03
Apagyi Dávid <apagyi.david@gmail.com>
2018-02-18 15:51:22 +0100

    Tobb aloldal, fajlfeltoltesnel tobb dolgot meg lehet adni

--- main.js
+++ main.js
@@ -10,17 +10,31 @@ app.use(fileUpload());
 //Feltöltött fájlok helye
 const dir = './uploads/';
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 
 //Főoldal
 app.get('/', (req, res) => {
   fs.readdir(dir, (err, files) => {
-    res.render('pages/index.ejs', {
+    res.render('pages/home.ejs', {
       files: files
     });
   });
 });
+
+app.get('/upload', (req, res) => {
+  res.render('pages/fileupload.ejs');
+});
+
+app.get('/search', (req, res) => {
+  fs.readdir(dir, (err, files) => {
+    res.render('pages/search.ejs', {
+      files: files
+    });
+  });
+});
+
+
 
 
 //A /file elérési úttal tudjuk elérni a feltöltött fájlokat
@@ -28,11 +42,11 @@ app.use('/file', express.static(dir));
 
 
 //Fájl feltöltése (html post)
-app.post('/upload', function(req, res) {
+app.post('/fileupload', function(req, res) {
   if (!req.files)
   return res.status(400).send('No files were uploaded.');
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  var sampleFile = req.files.sampleFile;
+  var sampleFile = req.files.files;
   // Fájl áthelyezése
   sampleFile.mv(dir+sampleFile.name, function(err) {
     if (err)
---
+++ views/partials/fileuploader/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/dapagyi/pen/RQxMRJ.
+
+ Simple upload UI animation  concept 
