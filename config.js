dbd6ef969b6223373947da92ca281dddf8471ebc
Apagyi Dávid <apagyi.david@gmail.com>
2018-03-21 01:21:06 +0100

    db

--- config.js
+++ config.js
@@ -2,16 +2,16 @@
 //https://medium.com/node-and-beyond/environment-dependent-node-js-configuration-b51149286e7e
 var config = {
     default: {
-      port: 3000,
-      databaseconnection: {
+        port: 3000,
+        databaseconnection: {
         host: "localhost",
         user: "nodeaccess",
         password: "b1T1fJAU",
         database: "dokumentor"
-      }
+        }
     }
-  }
-  
-  exports.get = function get(env) {
+}
+
+exports.get = function get(env) {
     return config[env] || config.default;
-  }
+}
