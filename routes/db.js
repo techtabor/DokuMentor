c40e72d8137f9e4cb81c410c7282de16120b1f52
Apagyi Dávid <apagyi.david@gmail.com>
2018-03-21 17:49:06 +0100

    db modul

--- routes/db.js
+++ routes/db.js
@@ -1,18 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+//Külső config fájl betöltése:
+var config = require('./config.js').get(process.env.NODE_ENV);
+
 var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(config.databaseconnection); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
 
 //Főoldal
 router.get('/all', (req, res) => {
-    //connection.connect();
-
     connection.query('SELECT * from new_view', function(err, rows, fields) {
     if (!err) fetchActors(res);
     else console.log('Error while performing Query.');
     });
-
-    //connection.end();
 });
 
 //TESZT:
@@ -45,9 +65,7 @@ function fetchActors(res){
     });
 }
 
-
-
-module.exports = function(database){
-    connection = database;
+module.exports = function(){
+    handleDisconnect();
     return router;
 }
