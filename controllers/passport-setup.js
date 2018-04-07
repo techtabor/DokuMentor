895a417b4dabbc77ccc56c4f5ed8b13a2cee9953
Apagyi Dávid <apagyi.david@gmail.com>
2018-04-08 01:04:34 +0200

    aws debug???

--- controllers/passport-setup.js
+++ controllers/passport-setup.js
@@ -16,7 +16,7 @@ passport.use(
     new GoogleStrategy({
         clientID: config.google.clientID,
         clientSecret: config.google.clientSecret,
-        callbackURL: '/auth/google/redirect'
+        callbackURL: 'http://ec2-18-195-57-19.eu-central-1.compute.amazonaws.com:3000/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
 
         User.findOrCreate({where: {key: profile.id}, defaults: {name: profile.displayName, email: profile.emails[0].value}})
