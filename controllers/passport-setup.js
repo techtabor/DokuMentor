a316ce3b6b99c100de24a610ec39fbfba5f38203
Apagyi Dávid <apagyi.david@gmail.com>
2018-04-08 00:25:59 +0200

    google komm megoldva

---
+++ controllers/passport-setup.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const config = require('../config/config.js').get(process.env.NODE_ENV);
+const User = require('../models/').users;
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findOne({where: {key: id.toString()}}).then((user) => {
+        done(null, user);
+    });
+});
+
+passport.use(
+    new GoogleStrategy({
+        clientID: config.google.clientID,
+        clientSecret: config.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, (accessToken, refreshToken, profile, done) => {
+
+        User.findOrCreate({where: {key: profile.id}, defaults: {name: profile.displayName, email: profile.emails[0].value}})
+        .spread(function(user, created) {
+            console.log(user.get({
+                plain: true
+            }))
+            console.log('Új?', created);
+            done(null, profile);
+        });
+    })
+);
