479a4112c5b3210e9252065f279f94e836621854
Apagyi Dávid <apagyi.david@gmail.com>
2018-04-22 20:04:21 +0200

    db insert documents, files

--- models/File.js
+++ models/File.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes){
      var File = sequelize.define('File', {
         size: {
           type: Sequelize.INTEGER,
-          allowNull: false
+          allowNull: true
         },
         extension: {
           type: Sequelize.STRING,
