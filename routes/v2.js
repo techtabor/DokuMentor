725777abab2fcd0e340fc9f719bbfa4af55de87a
Apagyi Dávid <apagyi.david@gmail.com>
2018-04-24 17:58:49 +0200

    documents

--- routes/v2.js
+++ routes/v2.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const models = require('../models/');
+
 const authCheck = (req, res, next) => {
     if(!req.isAuthenticated()){
         res.redirect('../auth/login');
@@ -15,6 +17,14 @@ router.get('/', (req, res) => {
 
 router.get('/newdocument', authCheck, (req, res) => {
     res.render('v2/pages/newdocument', { user: req.user });
+});
+
+router.get('/documents', (req, res) => {
+    models.Document.findAll().then( (result)=>{
+        console.log(result[0].dataValues);
+        //res.send(result);
+        res.render('v2/pages/documents', { user: req.user, documents: result});
+    });
 });
 
 module.exports = router;
